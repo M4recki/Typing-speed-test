cd35f908e47c10b68f79777783aa1246d0833ebc
M4recki <111280515+M4recki@users.noreply.github.com>
2023-05-24 18:05:30 +0200

    Added random sentences to enter in the appropriate field. To fix: counting words, characters per minute, % effectiveness and time counter

--- Typing Speed Test.py
+++ Typing Speed Test.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from faker import Faker
+import threading
 
 font_1 = ("Barlow", 27)
 
@@ -9,16 +10,66 @@ font_3 = ("Barlow", 15)
 
 gray = "#242424"
 
-# TODO: Add timer and code explanations
+# TODO: Add code explanations
 
 
 class App(ctk.CTk):
     def __init__(self):
+
+        def generate_random_sentence(self):
+            fake = Faker()
+            random_text = fake.text()
+            return random_text
+
+        def start_timer(self):
+            if self.seconds > 0:
+                self.seconds -= 1
+                self.time_counter_label["text"] = str(self.seconds)
+                self.time_counter_label.after(1000, self.start_timer)
+            else:
+                self.writing_area_entry.config(state="disabled")
+                self.analyze_data()
+
+        def count_words_per_minute(self, time_elapsed, typed_text):
+            words = typed_text.split()
+            words_per_minute = len(words) / (time_elapsed / 60)
+            return words_per_minute
+
+        def count_chars_per_minute(self, time_elapsed, typed_text):
+            chars_per_minute = len(typed_text) / (time_elapsed / 60)
+            return chars_per_minute
+
+        def calculate_accuracy(typed_text, original_text):
+            typed_chars = list(typed_text)
+            original_chars = list(original_text)
+            errors = 0
+            for i in range(len(typed_chars)):
+                if typed_chars[i] != original_chars[i]:
+                    errors += 1
+            accuracy = ((len(typed_chars) - errors) / len(typed_chars)) * 100
+            return accuracy
+
+        def start_analyzing_data(self):
+            if not threading.enumerate():
+                elapsed_time = 60 - self.seconds
+                self.words = count_words_per_minute(
+                    elapsed_time, self.writing_area_entry.get())
+                self.characters = count_chars_per_minute(
+                    elapsed_time, self.writing_area_entry.get())
+                self.accuracy = calculate_accuracy(
+                    self.writing_area_entry.get(), self.random_sentence)
+                self.words_per_minute_button["text"] = f"{self.words:.2f}"
+                self.chars_per_minute_button["text"] = f"{self.characters:.2f}"
+                self.accuracy_button["text"] = f"{self.accuracy:.2f}%"
+
+        # Attributes
         self.words = 0
         self.characters = 0
         self.accuracy = 0
         self.seconds = 60
+        self.random_sentence = generate_random_sentence(self)
 
+        # Window settings
         super().__init__()
         self.geometry("700x700")
         self.title("Typing speed test by Marek Baranski")
@@ -67,17 +118,14 @@ class App(ctk.CTk):
 
         self.time_border_label = ctk.CTkButton(self.data_frame, text="", width=120, height=95,
                                                bg_color=gray, hover_color="#E04E01", corner_radius=100, fg_color="#E04D01", border_color=gray)
-
         self.time_border_label.grid(row=4, column=1, pady=30, padx=100)
 
         self.time_counter_label = ctk.CTkButton(
             self.data_frame, text=self.seconds, font=font_2, text_color="white", width=1, height=60, corner_radius=100, fg_color="black", hover_color="black", border_spacing=4, bg_color="#E04D01")
-
         self.time_counter_label.grid(row=4, column=1, pady=30, padx=100)
 
         self.seconds_label = ctk.CTkLabel(
             self.data_frame, text="Seconds", font=font_3, text_color="white", fg_color=gray)
-
         self.seconds_label.grid(row=4, column=1, pady=(130, 0), padx=100)
 
         self.writing_area_frame = ctk.CTkFrame(
@@ -87,22 +135,17 @@ class App(ctk.CTk):
 
         self.writing_area_entry = ctk.CTkEntry(
             self.writing_area_frame, width=450, height=200, fg_color="#4e4e4e", corner_radius=10)
-
         self.writing_area_entry.grid(
             row=5, column=1, pady=25, padx=0, sticky="w")
+        self.writing_area_entry.bind(
+            "<KeyRelease>", lambda event: self.start_analyzing_data(self))
+        self.writing_area_entry.bind(
+            "<KeyRelease>", lambda event: self.start_timer(self))
 
-        self.sentence_label = ctk.CTkLabel(self)
-
-    def generate_random_sentence(self):
-        fake = Faker()
-        random_text = fake.text()
-        print(random_text)
-
-    def start_timer(self):
-        if self.seconds > 0:
-            self.seconds -= 1
-            self.time_counter_label["text"] = str(self.seconds)
-            self.time_counter_label.after(1000, self.start_timer)
+        self.sentence_label = ctk.CTkLabel(
+            self.writing_area_frame, text=self.random_sentence, font=font_3, fg_color="#4e4e4e", text_color="#E04D01", justify="center", wraplength=445)
+        self.sentence_label.grid(
+            row=5, column=1, padx=15, pady=30, sticky="nw")
 
 
 if __name__ == "__main__":
